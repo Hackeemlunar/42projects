2d0adde067003a57775dfe4d326290dbbf239019
Hackeem Mensah Bosu <hmensah-@lab1r6s7.42abudhabi.ae>
2024-12-19 19:39:51 +0400

    finished atoi

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -1,11 +1,52 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmensah- <hmensah-@student.42abudhabi.a    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/19 19:00:52 by hmensah-          #+#    #+#             */
+/*   Updated: 2024/12/19 19:39:22 by hmensah-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int ft_atoi(const char *str);
+#include "libft.h"
 
-int main(void)
+int	ft_isspace(char c)
 {
-    // Example test for ft_atoi
-    return 0;
+	if (c == ' ' || c == '\n' || c == '\f' || c == '\r'
+		|| c == '\v' || c == '\t')
+		return (1);
+	return (0);
 }
+
+int ft_atoi(const char *str)
+{
+    int res;
+    int sign;
+    int counter;
+
+    res = 0;
+    sign = 1;
+    counter = 0;
+    while (ft_isspace(str[counter]))
+        counter++;
+    if (str[counter] == '-')
+    {
+        sign *= -1;
+        counter++;
+    }
+    while(str[counter] <= '9' && str[counter] >= '0')
+    {
+        res = (res * 10) + (str[counter] - '0');
+        counter++;
+    }
+    return sign * res;
+}
+
+// int main(int argc, char **argv)
+// {
+//     argc += 0;
+//     printf("FT_atoi: %d\n", ft_atoi(argv[1]));
+//     printf("   Atoi: %d\n", atoi(argv[1]));
+// }
