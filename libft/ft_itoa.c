dfa5d5d31980325b00a68fcb37cccb2d63fd27a0
Hackeem Mensah Bosu <hmensah-@lab2r2s12.42abudhabi.ae>
2024-12-22 16:28:57 +0400

    just added itoa

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -1,11 +1,47 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmensah- <hmensah-@student.42abudhabi.a    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/22 16:09:25 by hmensah-          #+#    #+#             */
+/*   Updated: 2024/12/22 16:28:26 by hmensah-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-char *ft_itoa(int n);
+#include "libft.h"
 
-int main(void)
+char	*ft_itoa(int n)
 {
-    // Example test for *ft_itoa
-    return 0;
+	char	temp[11];
+	char	*res;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	res = (char *) malloc(12);
+	if (!res)
+		return (NULL);
+	if (n < 0)
+	{
+		res[j++] = '-';
+		n = -(n);
+	}
+	while (n > 0)
+	{
+		temp[i++] = (n % 10) + '0';
+		n /= 10;
+	}
+	while (i > 0)
+		res[j++] = temp[--i];
+	res[j] = '\0';
+	return (res);
+}
+
+int	main(void)
+{
+	printf("Numchar: %s\n", ft_itoa(0));
+	return (0);
 }
