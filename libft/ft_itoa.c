00f015d892396ec2f0ce68ddcae35cd30113e662
Hackeem M. Bosu <66512968+Hackeemlunar@users.noreply.github.com>
2024-12-22 16:43:55 +0400

    Did itoa

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -12,36 +12,90 @@
 
 #include "libft.h"
 
-char	*ft_itoa(int n)
-{
-	char	temp[11];
-	char	*res;
-	int		i;
-	int		j;
+#include "libft.h"
+#include <limits.h>
+#include <stdio.h>
 
-	i = 0;
-	j = 0;
-	res = (char *) malloc(12);
-	if (!res)
-		return (NULL);
-	if (n < 0)
-	{
-		res[j++] = '-';
-		n = -(n);
-	}
-	while (n > 0)
-	{
-		temp[i++] = (n % 10) + '0';
-		n /= 10;
-	}
-	while (i > 0)
-		res[j++] = temp[--i];
-	res[j] = '\0';
-	return (res);
+static char *handle_special_cases(int n)
+{
+    char *res;
+
+    if (n == 0)
+    {
+        res = (char *)malloc(2);
+        if (!res)
+            return (NULL);
+        res[0] = '0';
+        res[1] = '\0';
+        return (res);
+    }
+    if (n == INT_MIN)
+    {
+        res = (char *)malloc(13);
+        if (!res)
+            return (NULL);
+        ft_strlcpy(res, "-2147483648", 13);
+        return (res);
+    }
+    return (NULL); // Indicate not a special case
 }
 
-int	main(void)
+static char *convert_number(int n, int is_negative)
 {
-	printf("Numchar: %s\n", ft_itoa(0));
-	return (0);
+    char temp[12];
+    char *res;
+    int i = 0;
+    int j = 0;
+
+    while (n > 0)
+    {
+        temp[i++] = (n % 10) + '0';
+        n /= 10;
+    }
+    res = (char *)malloc(i + is_negative + 1);
+    if (!res)
+        return (NULL);
+    if (is_negative)
+        res[j++] = '-';
+    while (i > 0)
+        res[j++] = temp[--i];
+    res[j] = '\0';
+    return (res);
+}
+
+char *ft_itoa(int n)
+{
+    char *special_case_res;
+    int is_negative = 0;
+
+    special_case_res = handle_special_cases(n);
+    if (special_case_res)
+        return (special_case_res);
+    if (n < 0)
+    {
+        is_negative = 1;
+        n = -n;
+    }
+    return (convert_number(n, is_negative));
+}
+
+int main(void)
+{
+    int test_values[] = {0, -123, 123, INT_MIN, INT_MAX, -1, 1, 9, -9, 10, -10, 99, -99, 100, -100};
+    int num_tests = sizeof(test_values) / sizeof(test_values[0]);
+
+    for (int i = 0; i < num_tests; i++)
+    {
+        char *result = ft_itoa(test_values[i]);
+        if (result)
+        {
+            printf("ft_itoa(%d) = %s\n", test_values[i], result);
+            free(result);
+        }
+        else
+        {
+            printf("ft_itoa(%d) returned NULL\n", test_values[i]);
+        }
+    }
+    return (0);
 }
