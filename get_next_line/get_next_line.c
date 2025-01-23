2b5b6b21431a6beb4d1880d929395b562f29972f
Hackeem M. Bosu <66512968+Hackeemlunar@users.noreply.github.com>
2025-01-23 14:28:11 +0400

    started work on get_next_line

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,34 @@
+
+#include "get_next_line.h"
+
+char *get_next_line(int fd)
+{
+    static char	*line;
+    char		*buffer;
+    char		*temp;
+    int			read_bytes;
+
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return (NULL);
+    buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+    if (!buffer)
+        return (NULL);
+    read_bytes = read(fd, buffer, BUFFER_SIZE);
+    while (read_bytes > 0)
+    {
+        buffer[read_bytes] = '\0';
+        if (!line)
+            line = ft_strdup(buffer);
+        else
+        {
+            temp = ft_strjoin(line, buffer);
+            free(line);
+            line = temp;
+        }
+        if (ft_strchr(buffer, '\n'))
+            break ;
+        read_bytes = read(fd, buffer, BUFFER_SIZE);
+    }
+    free(buffer);
+    return (line);
+}
