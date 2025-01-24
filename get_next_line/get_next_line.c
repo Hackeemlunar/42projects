b035c414d4a85b4e09142cfa128e22f7732fe545
Hackeem Mensah Bosu <hmensah-@student.42abdhabi.ae>
2025-01-24 22:59:09 +0400

    Ready for debugging 1

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hmensah- <hmensah-@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 18:11:28 by hmensah-          #+#    #+#             */
-/*   Updated: 2025/01/24 21:36:36 by hmensah-         ###   ########.fr       */
+/*   Updated: 2025/01/24 22:43:59 by hmensah-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,10 @@ void	*ft_memset(void *b, int c, size_t len)
 	return (b);
 }
 
-int	check_bytes(t_line *line, int *buf_size)
+int	check_bytes(t_line *line)
 {
-	t_line	new_line;
-	int		i;
+	static t_line	new_line;
+	int				i;
 
 	i = 0;
 	while (line->temp[i] != '\n' && line->temp[i])
@@ -42,8 +42,7 @@ int	check_bytes(t_line *line, int *buf_size)
 		return (0);
 	}
 	ft_memset(&new_line, 0, sizeof(t_line));
-	*buf_size *= 2;
-	new_line.temp = malloc(*buf_size + 1);
+	new_line.temp = malloc(BUFFER_SIZE + 1);
 	if (!new_line.temp)
 		return (-1);
 	line->next = &new_line;
@@ -54,24 +53,30 @@ char	*extract_line(t_ldata *line_data)
 {
 	t_line	*temp_line;
 	char	*line;
+	char	*temp_ptr;
 	int		i;
 
 	temp_line = line_data->templn;
-	line = malloc(line_data->total_len + 1);
+	line = malloc(line_data->total_len + 2);
 	if (!line)
 		return (NULL);
 	i = 0;
-	while (i < line_data->total_len)
+	while (i < line_data->total_len && temp_line)
 	{
-		if (temp_line->temp && !(*temp_line->temp))
+		temp_ptr = temp_line->temp;
+		if (temp_ptr && !(*temp_ptr))
 		{
 			free(temp_line->temp);
 			temp_line = temp_line->next;
+			continue ;
 		}
-		if (temp_line->temp)
-			line[i] = *(temp_line->temp);
-		temp_line->temp++;
-		i++;
+		if (temp_ptr)
+		{
+			line[i++] = *temp_ptr;
+			temp_line->temp++;
+		}
+		else
+			temp_line = temp_line->next;
 	}
 	line[line_data->total_len] = '\n';
 	line[line_data->total_len + 1] = '\0';
@@ -82,32 +87,28 @@ char	*get_next_line(int fd)
 {
 	static t_ldata	ldata;
 	t_line			line;
-	int				buf_size;
 	int				bytes_result;
 
 	if (BUFFER_SIZE < 0 || fd < 0)
 		return (NULL);
-	buf_size = BUFFER_SIZE;
 	ft_memset(&line, 0, sizeof(t_line));
-	line.temp = malloc(buf_size + 1);
+	line.temp = malloc(BUFFER_SIZE + 1);
 	if (!line.temp)
 		return (NULL);
-	line.temp_len = read(fd, line.temp, buf_size);
+	line.temp_len = read(fd, line.temp, BUFFER_SIZE);
 	ldata.total_len = 0;
 	ldata.templn = &line;
 	while (line.temp_len > 0)
 	{
-		printf("%d\n", line.temp_len);
-		printf("%s\n", line.temp);
 		line.temp[line.temp_len] = '\0';
-		bytes_result = check_bytes(&line, &buf_size);
+		bytes_result = check_bytes(&line);
 		ldata.total_len += line.temp_len;
 		if (bytes_result == 0)
 			break ;
 		else if (bytes_result == -1)
 			return (NULL);
 		line = *line.next;
-		line.temp_len = read(fd, line.temp, buf_size);
+		line.temp_len = read(fd, line.temp, BUFFER_SIZE);
 	}
 	return (extract_line(&ldata));
 }
