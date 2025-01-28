bea0bf16a3fcd70b09e5891f3164ec2a72b714d2
Hackeem M. Bosu <66512968+Hackeemlunar@users.noreply.github.com>
2025-01-29 03:38:36 +0400

    Everything in copy, debugging time

--- get_next_line/copy.c
+++ get_next_line/copy.c
@@ -21,7 +21,7 @@
 #include <limits.h>
 
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE 2
+# define BUFFER_SIZE 1
 #endif
 
 typedef struct s_context
@@ -43,39 +43,47 @@ void *ft_strncpy(char *dst, const char *src, size_t n)
 
 	d = dst;
 	s = src;
-    while (n > 0 && *s) {
+    while (n > 0 && *s)
+    {
         *d++ = *s++;
         n--;
     }
-    while (n > 0) {
+    while (n > 0)
+    {
         *d++ = '\0';
         n--;
     }
     return dst;
 }
 
-void	create_context(t_context *ctx)
+void create_context(t_context *ctx)
 {
-	char	*buffer;
-
-	if (!ctx->buf_cap)
-		ctx->buf_cap = BUFFER_SIZE;
-	buffer =  malloc(ctx->buf_cap + 1);
-	if (!buffer)
-		return ;
-	buffer[0] = '\0';
-	ctx->buffer = buffer;
+    if (!ctx->buffer)
+    {
+        ctx->buf_cap = BUFFER_SIZE;
+        ctx->buffer = malloc(ctx->buf_cap + 1);
+        if (!ctx->buffer)
+            return ;
+        ctx->buffer[0] = '\0';
+    }
 }
 
-char	*handle_eof_err(t_context *ctx, ssize_t byt_read)
+char *handle_eof_err(t_context *ctx, ssize_t byt_read)
 {
     char *line;
 
-	if (byt_read < 0)
+    if (byt_read < 0)
     {
-        if (ctx->buffer)
-			free(ctx->buffer);
-        ctx = NULL;
+        free(ctx->buffer);
+        ctx->buffer = NULL;
+        ctx->buf_cap = 0;
+        return NULL;
+    }
+    if (ctx->stash_len + ctx->buf_pos == 0)
+    {
+        free(ctx->buffer);
+        ctx->buffer = NULL;
+        ctx->buf_cap = 0;
         return NULL;
     }
     line = malloc(ctx->stash_len + ctx->buf_pos + 1);
@@ -86,9 +94,12 @@ char	*handle_eof_err(t_context *ctx, ssize_t byt_read)
     if (ctx->buf_pos)
         ft_strncpy(line + ctx->stash_len, ctx->buffer, ctx->buf_pos);
     line[ctx->stash_len + ctx->buf_pos] = '\0';
-    if (ctx->buffer)
-		free(ctx->buffer);
-	ctx = NULL;
+    free(ctx->buffer);
+    ctx->buffer = NULL;
+    ctx->buf_cap = 0;
+    ctx->stash_len = 0;
+    ctx->buf_pos = 0;
+    ctx->buf_pos_prv = 0;
     return line;
 }
 
@@ -104,7 +115,8 @@ void	expland_buffer(t_context *ctx)
         return ;
     }
     ft_strncpy(new_buffer, ctx->buffer, ctx->buf_pos);
-    free(ctx->buffer);
+    if (ctx->buffer)
+        free(ctx->buffer);
     ctx->buffer = new_buffer;
 }
 
@@ -155,6 +167,29 @@ void	handle_line(t_context *ctx, ssize_t byt_read, char **line)
         expland_buffer(ctx);
 }
 
+void	handle_stash(t_context *ctx, char **line)
+{
+    size_t i;
+
+    i = 0;
+    while (i < ctx->stash_len)
+    {
+        if (ctx->stash[i] == '\n')
+        {
+            i++;
+            *line = malloc(i + 1);
+            if (!*line)
+                return ;
+            ft_strncpy(*line, ctx->stash, i);
+            (*line)[i] = '\0';
+            ft_strncpy(ctx->stash, ctx->stash + i, ctx->stash_len - i);
+            ctx->stash_len -= i;
+            return ;
+        }
+        i++;
+    }
+}
+
 char	*get_next_line(int fd)
 {
 	static t_context	ctx;
@@ -168,6 +203,12 @@ char	*get_next_line(int fd)
     ctx.nl_err = 0;
 	while (1)
     {
+        if (ctx.stash_len)
+        {
+            handle_stash(&ctx, &line);
+            if (line)
+                return line;
+        }
         byt_read = read(fd, ctx.buffer + ctx.buf_pos, BUFFER_SIZE);
         if (byt_read <= 0)
 			return (handle_eof_err(&ctx, byt_read));
@@ -183,7 +224,7 @@ char	*get_next_line(int fd)
 // 	int fd;
 // 	char *line;
 
-// 	fd = open("test1.txt", O_RDONLY);
+// 	fd = open("test.txt", O_RDONLY);
    
 // 	line = get_next_line(fd);
 // 	printf("%s", line);
