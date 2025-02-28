4dba2cdedcb7cd6b288a27746b989310a94a9ea6
Hackeem Mensah Bosu <66512968+Hackeemlunar@users.noreply.github.com>
2025-02-28 14:31:30 +0400

    done with linux setup

--- fractol/fractol.c
+++ fractol/fractol.c
@@ -1,33 +1,58 @@
 #include "mlx_mac/mlx.h"
 
-typedef struct	s_idata {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_idata;
+typedef struct  s_data {
+    void    *img;
+    char    *addr;
+    int     bits_per_pixel;
+    int     line_length;
+    int     endian;
+    void    *mlx;
+    void    *mlx_win;
+}   t_data;
 
-void	my_mlx_pixel_put(t_idata *data, int x, int y, int color)
+void    my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
-	char	*dst;
+    char    *dst;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
+    dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+    *(unsigned int*)dst = color;
 }
 
-int	main(void)
+void    fill_image(t_data *data, int color)
 {
-	void	*mlx;
-	void	*mlx_win;
-	t_idata	img;
+    int x;
+    int y;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 620, 800, "Hello world!");
-	img.img = mlx_new_image(mlx, 520, 700);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-								&img.endian);
-	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
+    y = 0;
+    while (y < 800)
+    {
+        x = 0;
+        while (x < 800)
+        {
+            my_mlx_pixel_put(data, x, y, color);
+            x++;
+        }
+        y++;
+    }
+}
+
+int expose_hook(t_data *data) {
+    int color = 0x00590929;
+    fill_image(data, color);
+    mlx_put_image_to_window(data->mlx, data->mlx_win, data->img, 0, 0);
+    return 0;
+}
+
+int main(void)
+{
+    t_data  data;
+
+    data.mlx = mlx_init();
+    data.mlx_win = mlx_new_window(data.mlx, 800, 800, "Fractol");
+    data.img = mlx_new_image(data.mlx, 800, 800);
+    data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_length, &data.endian);
+
+    mlx_expose_hook(data.mlx_win, expose_hook, &data);
+    expose_hook(&data); // Initial draw.
+    mlx_loop(data.mlx);
 }
